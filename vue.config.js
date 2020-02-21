0c89054bb0ab71bd7edbdd5861afa720c92e1e4b
Mitch Samuels <samuels.mitch@gmail.com>
2020-02-20 23:51:17 -0600

    Changed public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'
-	  ? '/vuefirebase/' 
+	  ? '/' 
 	  // Change this ^^ path if the app won't be at root level ie website.com/app/ - put "/app/"
 	  : '/'
   }
